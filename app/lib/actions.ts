c52fa35358ba200ce49c42956e4f31ec40501b21
孝扬 <734510805@qq.com>
2025-09-02 08:31:30 +0800

    feat: revalidate and redirect

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,19 @@
+/*
+ * @Author: 孝扬
+ * @Date: 2025-09-02 08:21:46
+ * @LastEditors: 孝扬
+ * @LastEditTime: 2025-09-02 08:27:51
+ * @Version: V1.0
+ * @Description:
+ */
 "use server";
 
 import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
@@ -21,5 +34,12 @@ export async function createInvoice(formData: FormData) {
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
-  console.log("🚀 ~ createInvoice ~ date:", date);
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
